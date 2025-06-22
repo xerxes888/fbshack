639d89a9f063b7c3d4725ac56cd2942c826dc839
Termux Script <radius.monim@gmail.com>
2025-06-22 17:28:46 +0900

    Update x888.sh

--- x888.sh
+++ x888.sh
@@ -2,53 +2,52 @@
 clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
-pur='\033[35;1m' #purpl
+pur='\033[35;1m' #purple
 cy='\033[36;1m' #cyan
 me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-# Please Don't Recode
+# Jangan Recode Yah Yauw !
 echo
 echo
 echo
-echo $i"["$bi"•"$i"]"$me"───────────────────────────"$i"["$bi"•"$i"]"
-echo $i"["$bi"•"$i"]"$me"───────────────────────────"$i"["$bi"•"$i"]"
-echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE            "$i"|"
-echo $i" |"$me"────────────────────────────────────────"$i"|"
-echo $i" |"$pu" AUTHOR :"$ku" XERXES  888251  [FR13ND8]        "$i"|"
-echo $i" |"$pu" FACEBOOK :"$ku"   RY Monim                     "$i"|"
-echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"|"
-echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]    "$i"|"
-echo $i" |"$pu" Contack WhatsApp :"$ku"0823988242__            "$i"|"
-echo $i" |"$pu" Contack Gmail :"$ku"monim26rady@gmail.com"$i"      |"
-echo $i"["$bi"•"$i"]"$me"───────────────────────────"$i"["$bi"•"$i"]"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE"$i"         |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" RADIUS Y MONIM  [FR13ND8]"$i"          |"
+echo $i" |"$pu" FACEBOOK :"$ku" RY Monim"$i"                   |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
+echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]"$i" |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 0823988242__"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" radius.monim@gmail.com"$i"      |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
-echo $i"─────────────────────────────────────────"
-echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP     "$i"|"$i"    |"$cy" R"$i" |"
-echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3 "$i"|"$i"    |"$cy" A"$i" |"
-echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz  "$i"|"$i"    |"$cy" D"$i" |"
-echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF     "$i"|"$i"    |"$cy" I"$i" |"
-echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420    "$i"|"$i"    |"$cy" U"$i" |"
-echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB (root)  "$i"|"$i"    |"$cy" S"$i" |"
-echo $i"────────────────────────────────"$i"             
-echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK"$i"|"$i"    ─────"
-echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK"$i"|"$i"    |"$cy" Y"$i" |"
-echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK  "$i"|"$i"    
-echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK"$i"|"$i"    |"$cy" M"$i" |"
-echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK  "$i"|"$i"    |"$cy" O"$i" |"
-echo $i"|"$me"12"$i" |"$cy" BOT COMENT FACEBOOK    "$i"|"$i"    |"$cy" N"$i" |"
-echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK     "$i"|"$i"    |"$cy" I"$i" |"
-echo $i"──────────────────────────────"$i"               |"$cy" M"$i" |"
-echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"
-echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" D"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" U"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" S"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" -"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" Y"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" -"$i" |"
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" -"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" M"$i" |"
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" I"$i" |"
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" M"$i" |"
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
 echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" H"$i" |"
 echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" A"$i" |"
-echo $i"────────────────────────────────"$i"                |"$cy" C"$i" |"
-echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" C"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU YAUW  "$i"|"$i"    |"$cy"K"$i" |"
 echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" S"$i" |"
 echo $i"────────────────────────────────    ─────"
 echo
-echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomor""$bi"]"
 echo $me"¦"
 read -p"└──# " pil
 
